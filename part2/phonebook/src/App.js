a68839657aaf440fb6e1d2877d22492254b89026
adnanakram01 <eternitylord@protonmail.com>
2021-06-04 18:26:38 +0530

    Added effect hook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Filter = (props) => {
     return (
@@ -40,13 +41,16 @@ const Persons = ({namesToShow}) => {
 
 const App = () => {
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
 
+  const [persons, setPersons] = useState([])
+
+  const hook = () => {
+    axios.get('http://localhost:3001/persons').then(res => {
+      setPersons(res.data)
+    })
+  }
+
+  useEffect(hook, [])
 
   const names = persons.map(person => person.name)
   const [newName, setNewName] = useState('')
